a69bc1fb7310e193f097ed6ba99575c38ab721ce
Kays0K <kayson.games.youtub@gmail.com>
2026-03-02 11:51:06 -0300

    conectando no sql : )

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import Fastify from "fastify";
+import { Pool } from 'pg';
+
+const sql = new Pool({
+    user: "postgres",
+    password: "1234",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
 
 const server = Fastify();
 
-server.get('/usuarios', () => {
-    return 'Ta funfando poha!!!!'
+server.get('/usuarios', async () => {
+    const result = await sql.query('SELECT * FROM usuario');
+
+    return result.rows;
+});
+
+server.post('/usuarios', async (request, reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',[nome, senha]);
+    return 'Usuario criado com sucesso';
 });
 
 server.listen({ 
