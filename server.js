a9bf42bde0b91e0a313b4c2325dc4a7dd0c15f59
Kays0K <kayson.games.youtub@gmail.com>
2026-03-06 08:20:11 -0300

    Deletar usuários!

--- server.js
+++ server.js
@@ -21,14 +21,20 @@ server.post('/usuarios', async (request, reply) => {
     const nome = request.body.nome;
     const senha = request.body.senha;
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',[nome, senha]);
-    return 'Usuario criado com sucesso';
+    return 'Usuario criado com sucesso!';
 });
 
 server.put('/usuarios/:id', async (request, reply) => {
     const body = request.body;
     const id = request.params.id;
     const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3',[body.nome, body.senha, id])
-    return 'Usuário alterado com sucesso';
+    return 'Usuário alterado com sucesso!';
+});
+
+server.delete('/usuarios/:id', async (request, reply) => {
+    const id = request.params.id;
+    const resultado = await sql.query('DELETE FROM usuario WHERE id = $1', [id]);
+    return 'Usuário deletado!'
 });
 
 server.listen({ 
